cbeafad3df674002d2a5bc2c462d9eeed891d143
Tim Ryan <timryanhimself@gmail.com>
2022-11-29 15:17:29 +0000

    array problem

--- Program.cs
+++ Program.cs
@@ -1,12 +1,65 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
-namespace c_sharp_repo
+class Result
 {
-    class Program
+
+    /*
+     * Complete the 'missingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY arr
+     *  2. INTEGER_ARRAY brr
+     */
+
+    public static List<int> missingNumbers(List<int> arr, List<int> brr)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        List<int> myAnswer = new List<int>();
+        return myAnswer;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter("./output.log", true);
+
+        
+        string[] lines = File.ReadAllLines("input.txt");
+
+        int n = Convert.ToInt32(lines[0].Trim());
+
+        List<int> arr = lines[1].TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int m = Convert.ToInt32(lines[2].Trim());
+
+        List<int> brr = lines[3].TrimEnd().Split(' ').ToList().Select(brrTemp => Convert.ToInt32(brrTemp)).ToList();
+
+        List<int> result = Result.missingNumbers(arr, brr);
+
+
+        Console.WriteLine("n is {0}",n);
+
+
+
+        textWriter.WriteLine(String.Join(" ", result));
+
+        textWriter.Flush();
+        textWriter.Close();
     }
 }
